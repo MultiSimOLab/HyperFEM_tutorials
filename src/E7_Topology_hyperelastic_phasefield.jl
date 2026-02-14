2592c9433d678914582b2704a7955d1d7940d8a8
Jesus Martínez <jesus.martinez@upct.es>
2026-02-14 09:36:35 +0000

    Refactors topology optimization to use phase-field
    Renames and refactors the topology optimization script to employ a phase-field method.
    This includes changes to the physical model, density initialization using multiple shapes,
    boundary filtering, and the adjoint problem formulation. Modifies the load application
    and updates the optimization functionals for compliance and volume.

--- src/E7_Topology_hyperelastic_phasefield.jl
+++ src/E7_Topology_hyperelastic_phasefield.jl
@@ -1,17 +1,19 @@
-using Gridap, GridapGmsh, HyperFEM, HyperShape, GridapSolvers, DrWatson
-using TimerOutputs
+using Gridap, HyperFEM, HyperShape, GridapSolvers, DrWatson
 using GridapSolvers.NonlinearSolvers
-using HyperShape:evaluate!
+using GridapSolvers.LinearSolvers
+using Gridap.FESpaces
 using WriteVTK
+using HyperShape:evaluate!
 
-pname = "CantileverNeuman_density"
+function run()
+pname = "CantileverNeumann_phasefield"
 simdir = datadir("sims", pname)
 setupfolder(simdir)
 
-xmax=3.0
-ymax=1.0
+xmax = 3.0
+ymax = 1.0
 domain = (0, xmax, 0, ymax)
-N = 30
+N = 80
 h = 1 / N
 partition = (3N, N)
 geomodel = CartesianDiscreteModel(domain, partition)
@@ -21,92 +23,109 @@ labels = get_face_labeling(geomodel)
 add_tag_from_tags!(labels, "diri_0", [1, 3, 7])
 add_tag_from_tags!(labels, "Force", [2, 4, 8])
 
-
 physmodel_s = MooneyRivlin2D(λ=3.0, μ1=1.0, μ2=0.0, ρ=1.0)
-physmodel_v = MooneyRivlin2D(λ=3.0 * 1e-6, μ1=1.0 * 1e-6, μ2=0.0, ρ=1.0)
+physmodel_v = LinearElasticity2D(λ=3.0 * 1e-3, μ=1.0 * 1e-3, ρ=1.0)
 physmodel = EnergyInterpolationScheme(physmodel_s, physmodel_v; p=3.0)
 
 _, ∂Ψ, ∂2Ψ, _, D∂Ψ_Dρ, _ = physmodel()
 
 # Setup integration
 order = 1
-degree = 2 * order
+degree = 4 * order
 Ω = Triangulation(geomodel)
 dΩ = Measure(Ω, degree)
 Γ = BoundaryTriangulation(geomodel, tags="Force")
 dΓ = Measure(Γ, degree)
 
 # Dirichlet conditions 
-evolu(Λ) = 1.0
 dir_u_tags = ["diri_0"]
 dir_u_values = [[0.0, 0.0]]
-dir_u_timesteps = [evolu]
-D_bc = DirichletBC(dir_u_tags, dir_u_values, dir_u_timesteps)
+D_bc = DirichletBC(dir_u_tags, dir_u_values)
 
 #  FE elements 
 reffeu = ReferenceFE(lagrangian, VectorValue{2,Float64}, order)
-refρL2 = ReferenceFE(lagrangian, Float64, 0)
 refρ = ReferenceFE(lagrangian, Float64, 1)
 
-V = TestFESpace(geomodel, reffeu, D_bc, conformity=:H1)
-VρL2 = TestFESpace(Ω, refρL2, NothingBC(), conformity=:L2)
-Vρ = TestFESpace(Ω, refρ, NothingBC(), conformity=:H1)
-U = TrialFESpace(V, D_bc)
+V = TestFESpace(geomodel, reffeu, D_bc)
+Vρ = TestFESpace(Ω, refρ, NothingBC())
+U = TrialFESpace(V, D_bc, 1.0)
 
 # ************************************************************************************************
 #                                 Density initialization
 # ************************************************************************************************
-ρinit_ = Ellipsoid([0.5, 0.5], [0.2, 0.2])
-ρinit(x) = 1.0 + 0.0 * ρinit_(x)
-ρh = interpolate_everywhere(ρinit, VρL2) # variable en L2
+Number_holes = [7, 3]
+radius_holes = 0.1
+
+x = range(0.15, stop=xmax - 0.1, length=Number_holes[1])  # Posiciones en x
+y = range(0.15, stop=ymax - 0.15, length=Number_holes[2])  # Posiciones en y
+
+centros = [collect(c) for c in reshape(collect(Iterators.product(x, y)), Number_holes[1], Number_holes[2])]  # Generar matriz de vectores
+
+centros = centros[:]
+shapes = Vector{Any}(map((x) -> Ellipsoid(x, [radius_holes, radius_holes]), centros))
+box1 = Box([0.0, 0.0], [2.5 * radius_holes, ymax])
+box2 = Box([xmax - radius_holes, 0.0], [radius_holes + 0.1, ymax])
+
+push!(shapes, box1)
+push!(shapes, box2)
+
+ρinit(y) = 1.0 - sum(map((x) -> x(y), shapes[1:end-2])) + sum(map((x) -> x(y), shapes[end-2:end]))
+
+ρh = interpolate_everywhere(ρinit, Vρ) # variable en L2
+ρh_ = get_free_dof_values(ρh)
+ρh_ .= map((x) -> max(0.0, min(x, 1.0)), ρh_)
+
+fileI_seed = datadir("sims", pname) * "/seed" * ".vtu"
+writevtk(Ω, fileI_seed, cellfields=["ρh" => ρh])
+
 
 # ************************************************************************************************
-#                                 Filter density
+#                                 Boundary FILTER
 # ************************************************************************************************
 
-ε = 3h
+ε = 2h
 a_filter(u, v) = ∫(ε^2 * (∇(v) ⋅ ∇(u)) + v * u) * dΩ
-l_filter(v) = ∫((ρh * v)) * dΩ
-FILTER = StaticLinearModel(l_filter, a_filter, Vρ, Vρ, D_bc)
+FILTER = StaticLinearModel(a_filter, Vρ, Vρ, D_bc)
 
-ρfh = get_state(FILTER)
 
 # ************************************************************************************************
 #                                 Forward Problem
 # ************************************************************************************************
+
 km = Kinematics(Mechano, Solid)
 F, _, _ = get_Kinematics(km)
 ΔDirac(ϵ, x₁, x₂) = (x) -> (x[1] ≥ x₁ - ϵ && x[1] ≤ x₁ + ϵ && x[2] ≥ x₂ - ϵ && x[2] ≤ x₂ + ϵ) ? 1.0 : 0.0
-g(Λ) = (x) -> ΔDirac(0.1, xmax, ymax / 2)(x) * VectorValue(0.0, -Λ * 1e-3)
-res(Λ) = (u, v) -> ∫(∇(v)' ⊙ (∂Ψ ∘ (ρfh, F ∘ (∇(u)'))))dΩ - ∫(g(Λ) ⋅ v)dΓ
-jac(Λ) = (u, du, v) -> ∫(∇(v)' ⊙ ((∂2Ψ ∘ (ρfh, F ∘ (∇(u)'))) ⊙ (∇(du)')))dΩ
+g(Λ) = (x) -> ΔDirac(0.1, xmax, ymax / 2)(x) * VectorValue(0.0, -Λ * 5e-3)
+res(Λ) = (u, v) -> ∫(∇(v)' ⊙ (∂Ψ ∘ (ρh, F ∘ (∇(u)'))))dΩ - ∫(g(Λ) ⋅ v)dΓ
+jac(Λ) = (u, du, v) -> ∫(∇(v)' ⊙ ((∂2Ψ ∘ (ρh, F ∘ (∇(u)'))) ⊙ (∇(du)')))dΩ
+
 ls = LUSolver()
-nls_ = NewtonSolver(ls; maxiter=20, atol=1.e-10, rtol=1.e-8, verbose=false)
- 
+nls_ = NewtonSolver(ls; maxiter=20, atol=1.e-8, rtol=1.e-6, verbose=false)
 FORWARD = StaticNonlinearModel(res, jac, U, V, D_bc; nls=nls_)
 
 uh = get_state(FORWARD)
-
+ 
 # ************************************************************************************************
 #                                 Adjoint Problem
 # ************************************************************************************************
 
 a(p, v) = jac(1.0)(uh, p, v)
 l(v) = ∫(g(1.0) ⋅ v)dΓ
-ls = LUSolver()
 ADJOINT = StaticLinearModel(l, a, V, V, D_bc)
 
 ph = get_state(ADJOINT)
+
+
 # ************************************************************************************************
 #                                 Optimisation functionals
 # ************************************************************************************************
 
 J_(u, ϕ) = ∫(g(1.0) ⋅ u)dΓ
-DJ_(u, p, ϕ) = (v) -> (-1.0) * ∫(((D∂Ψ_Dρ ∘ (ϕ, F ∘ (∇(u)'))) ⊙ (∇(p)')) * v)dΩ
-Compliance = FEFunctional(J_, DJ_, uh, ph, ρfh)
+DJ_(u, p, ϕ) = (v) -> (-1.0) * ∫((((D∂Ψ_Dρ ∘ (ϕ, F ∘ (∇(u)'))) ⊙ (∇(p)')) * ρh * (1.0 - ρh)) * v)dΩ
+Compliance = FEFunctional(J_, DJ_, uh, ph, ρh)
 
 Vol_(u, ϕ) = ∫(ϕ)dΩ
-DVol_(u, p, ϕ) = (v) -> ∫(v)dΩ
+DVol_(u, p, ϕ) = (v) -> ∫(ρh * (1.0 - ρh) * v)dΩ
 Volume = FEFunctional(Vol_, DVol_, nothing, nothing, ρh)
 
 # ************************************************************************************************
@@ -114,22 +133,22 @@ Volume = FEFunctional(Vol_, DVol_, nothing, nothing, ρh)
 # ************************************************************************************************
 
 tol = 1e-3
-λ = 3000
-t = 1
+λ = 10
+t = 0.24
 V0 = ∑(∫(1.0)dΩ)
-pvd_topo = paraview_collection(simdir, append=false)
+filePath = datadir("sims", pname)
+pvd_topo = paraview_collection(filePath, append=false)
 
-PDES = [FILTER, FORWARD, ADJOINT]
+PDES = [FORWARD, ADJOINT]
 
 options = Dict(
-  FILTER => NamedTuple(),
-  FORWARD => (stepping=(nsteps=5, maxbisec=5),),
+  FORWARD => (stepping=(nsteps=1, maxbisec=5),),
   ADJOINT => NamedTuple(),
 )
 
 ρv = get_free_dof_values(ρh)
 
-  for i_top in 1:400
+  for i_top in 1:200
 
     # SOLVE PDEs
     map(x -> solve!(x; options[x]...), PDES)
@@ -141,26 +160,29 @@ options = Dict(
     if i_top == 1
       J, dJ = adimensionalize!(Compliance, J)
       Vol, dVol = adimensionalize!(Volume, V0)
-      # options[FORWARD]=(stepping=(nsteps=1, maxbisec=5),ProjectDirichlet=false)
+      options[FORWARD] = (stepping=(nsteps=1, maxbisec=5),)
     end
 
     FILTER(dJ)
-
-    DJh = FEFunction(Vρ, dJ)
-    DJh_L2 = interpolate_everywhere(DJh, VρL2)
-    DJL2 = get_free_dof_values(DJh_L2)
+    FILTER(dVol)
 
     Jt = J + λ * Vol
-    DJt = DJL2 + λ * dVol
+    DJt = dJ + λ * dVol
+
     ρv .-= t * DJt
     ρv .= map((x) -> max(0.0, min(x, 1.0)), ρv)
     @show J, Vol, Jt, norm(DJt), minimum(ρv), maximum(ρv)
+    DJht = FEFunction(Vρ, DJt)
     if norm(DJt) < tol
       break
     else
-      fileI = simdir * "/iter_$i_top" * ".vtu"
-      pvd_topo[i_top] = createvtk(Ω, fileI, cellfields=["uh" => uh, "ρh" => ρh, "DJh" => DJt])
+      fileI = filePath * "/iter_$i_top" * ".vtu"
+      pvd_topo[i_top] = createvtk(Ω, fileI, cellfields=["uh" => uh, "ρh" => ρh, "DJh" => DJht])
     end
   end
   WriteVTK.vtk_save(pvd_topo)
+
+end
+
+run()
 
